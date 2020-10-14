feaeb6ed511d781ec25a7fa6ddf52c481a769e09
Tamires Pereira <tamirespereira6@gmail.com>
2020-10-14 19:21:24 -0300

    login facebook

--- app/src/main/java/com/example/mesanews/feature/login/LoginActivity.kt
+++ app/src/main/java/com/example/mesanews/feature/login/LoginActivity.kt
@@ -14,10 +14,18 @@ import com.example.mesanews.feature.cadastro.CadastroActivity
 import com.example.mesanews.feature.home.HomeActivity
 import com.example.mesanews.util.Constants
 import com.example.mesanews.util.Constants.TOKEN
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity(), LoginContract.View {
     private val presenter = LoginPresenter()
+    private lateinit var callbackManager: CallbackManager
 
     private val sp: SharedPreferences by lazy {
         getSharedPreferences(Constants.TOKEN_SAVE, Context.MODE_PRIVATE)
@@ -27,6 +35,30 @@ class LoginActivity : AppCompatActivity(), LoginContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
+
+        callbackManager = CallbackManager.Factory.create()
+        login_button_facebook.setReadPermissions("email")
+
+        login_button_facebook.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(loginResult: LoginResult?) {
+                    val token = loginResult?.accessToken?.token
+                    if (token != null) {
+                        goToHome(token)
+                    }
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(exception: FacebookException) {
+
+                }
+            })
         presenter.view = this
 
         btn_register.setOnClickListener {
@@ -35,7 +67,12 @@ class LoginActivity : AppCompatActivity(), LoginContract.View {
 
         button_enter.setOnClickListener {
             if (verifyFields())
-                presenter.signIn(UserLogin(edit_login.text.toString(), edit_password.text.toString()))
+                presenter.signIn(
+                    UserLogin(
+                        edit_login.text.toString(),
+                        edit_password.text.toString()
+                    )
+                )
             else
                 showDialog(getString(R.string.fields))
         }
@@ -82,4 +119,11 @@ class LoginActivity : AppCompatActivity(), LoginContract.View {
     override fun verifyFields(): Boolean{
         return edit_login.text.isNotEmpty() && edit_password.text.isNotEmpty()
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
